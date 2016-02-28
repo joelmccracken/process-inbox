f3599467fbd3a23fecdcba058ab5fd8295504234
Joel McCracken <mccracken.joel@gmail.com>
2016-02-27 21:14:33 -0500

    simplify, just use dired

--- process-inbox.el
+++ process-inbox.el
@@ -1,31 +1,13 @@
-
+;; -*- lexical-binding: t -*-
 
 (defun process-inbox ()
   "start processing the inbox directory"
   (interactive)
-  (let (
-        (inbox-directory (expand-file-name "~/Inbox"))
-        (pi-buffer (get-buffer-create "*process-inbox*"))
-        (ls-process  (start-process "process-inbox-ls"
-                                    nil
-                                    "ls"
-                                    inbox-directory))
-        )
-    (delete-other-windows)
-    (set-process-filter ls-process
-                        (lambda (process output)
-                          (with-current-buffer (get-buffer-create "*process-inbox*")
-                            (goto-char (point-max))
-                            (insert output))))
-    (set-process-sentinel ls-process
-                          (lambda (process event)
-                            (when (string= event "finished\n")
-                              (process-inbox-mode)
-                              )
-                            ))
-    ))
+  (dired "~/Inbox")
+  (rename-buffer "*process-inbox*")
+  (inbox-dired-mode))
 
-
+(kill-buffer "Inbox")
 (setq process-inbox--internal-extensions
       '("txt"
         "pdf"
@@ -33,20 +15,9 @@
         "gif"
         ))
 
-
-(define-derived-mode process-inbox-mode special-mode
-  "Process Inbox"
-
-  "Major mode for inbox directory processing"
-  )
-
-(define-key proces-inbox-mode-map
-  (kbd "RET")
-
-  )
-
 (defun process-inbox/visit-file ()
-
+  "visit a file as part of processing it"
+  (interactive)
 
     )
 
@@ -58,8 +29,6 @@
 
 
 (define-key inbox-dired-mode-map
-  (kbd)
+  (kbd "RET")
+  'process-inbox/visit-file
   )
-
-
-(define )
